c273b56729fb354443aca5681734c8576c0209e8
Karthikeyan Kasinathan <karthikk@acclaimsystems.com>
2021-03-26 20:16:28 -0400

    Add project files.

---
+++ DocumentManager.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DocumentManager.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
