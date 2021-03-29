62e27da06c834edc42d8344bd527684fa68bd87f
dev-thinks <dev-thinks@github.com>
2021-03-29 16:30:55 -0400

    OpenXmlDocxRef from open xml sdk tool.

--- DocumentManager.Core/Converters/Handlers/DocxWatermark.cs
+++ DocumentManager.Core/Converters/Handlers/DocxWatermark.cs
@@ -8,6 +8,7 @@ using DocumentManager.Core.Models;
 using Microsoft.Extensions.Logging;
 using System.IO;
 using System.Linq;
+using DocumentManager.Core.MailMerge;
 using HorizontalAnchorValues = DocumentFormat.OpenXml.Vml.Wordprocessing.HorizontalAnchorValues;
 using Lock = DocumentFormat.OpenXml.Vml.Office.Lock;
 using VerticalAnchorValues = DocumentFormat.OpenXml.Vml.Wordprocessing.VerticalAnchorValues;
@@ -73,9 +74,6 @@ namespace DocumentManager.Core.Converters.Handlers
             if (!doc.MainDocumentPart.HeaderParts.Any())
             {
                 doc.MainDocumentPart.DeleteParts(doc.MainDocumentPart.HeaderParts);
-                var newHeaderPart = doc.MainDocumentPart.AddNewPart<HeaderPart>();
-                var rId = doc.MainDocumentPart.GetIdOfPart(newHeaderPart);
-                var headerRef = new HeaderReference {Id = rId};
 
                 var sectionProps = doc.MainDocumentPart.Document.Body.Elements<SectionProperties>().LastOrDefault();
                 if (sectionProps == null)
@@ -84,176 +82,24 @@ namespace DocumentManager.Core.Converters.Handlers
                     doc.MainDocumentPart.Document.Body.Append(sectionProps);
                 }
 
-                sectionProps.RemoveAllChildren<HeaderReference>();
-                sectionProps.Append(headerRef);
+                HeaderPart headerPart1 = doc.MainDocumentPart.AddNewPart<HeaderPart>("rId7");
+                OpenXmlDocxRef.GenerateHeaderPart1Content(headerPart1);
+                var rId1 = doc.MainDocumentPart.GetIdOfPart(headerPart1);
+                var headerRef1 = new HeaderReference { Id = rId1 };
+                sectionProps.Append(headerRef1);
 
-                newHeaderPart.Header = MakeHeader();
-                newHeaderPart.Header.Save();
+                HeaderPart headerPart2 = doc.MainDocumentPart.AddNewPart<HeaderPart>("rId6");
+                OpenXmlDocxRef.GenerateHeaderPart2Content(headerPart2);
+                var rId2 = doc.MainDocumentPart.GetIdOfPart(headerPart2);
+                var headerRef2 = new HeaderReference { Id = rId2 };
+                sectionProps.Append(headerRef2);
+
+                HeaderPart headerPart3 = doc.MainDocumentPart.AddNewPart<HeaderPart>("rId10");
+                OpenXmlDocxRef.GenerateHeaderPart3Content(headerPart3);
+                var rId3 = doc.MainDocumentPart.GetIdOfPart(headerPart3);
+                var headerRef3 = new HeaderReference { Id = rId3 };
+                sectionProps.Append(headerRef3);
             }
-
-            foreach (HeaderPart headerPart in doc.MainDocumentPart.HeaderParts)
-            {
-                var sdtBlock = new SdtBlock();
-                var sdtProperties = new SdtProperties();
-                var sdtId = new SdtId() {Val = 87908844};
-
-                var sdtContentDocPartObject = new SdtContentDocPartObject();
-                var docPartGallery = new DocPartGallery() {Val = "Watermarks"};
-                var docPartUnique = new DocPartUnique();
-
-                sdtContentDocPartObject.Append(docPartGallery);
-                sdtContentDocPartObject.Append(docPartUnique);
-                sdtProperties.Append(sdtId);
-                sdtProperties.Append(sdtContentDocPartObject);
-
-                var sdtContentBlock = new SdtContentBlock();
-                var paragraph = new Paragraph()
-                {
-                    RsidParagraphAddition = "00656E18",
-                    RsidRunAdditionDefault = "00656E18"
-                };
-
-                var paragraphProperties = new ParagraphProperties();
-                var paragraphStyleId = new ParagraphStyleId() {Val = "Header"};
-                paragraphProperties.Append(paragraphStyleId);
-
-                var run1 = new Run();
-                var runProperties = new RunProperties();
-                var noProof = new NoProof();
-                runProperties.Append(noProof);
-                var picture = new Picture();
-                var shapeType = new Shapetype
-                {
-                    Id = WaterMarkTypeId,
-                    CoordinateSize = "21600,21600",
-                    OptionalNumber = 136,
-                    Adjustment = "10800",
-                    EdgePath = "m@7,l@8,m@5,21600l@6,21600e"
-                };
-
-                var formulas = new Formulas();
-                var formula1 = new Formula() {Equation = "sum #0 0 10800"};
-                var formula2 = new Formula() {Equation = "prod #0 2 1"};
-                var formula3 = new Formula() {Equation = "sum 21600 0 @1"};
-                var formula4 = new Formula() {Equation = "sum 0 0 @2"};
-                var formula5 = new Formula() {Equation = "sum 21600 0 @3"};
-                var formula6 = new Formula() {Equation = "if @0 @3 0"};
-                var formula7 = new Formula() {Equation = "if @0 21600 @1"};
-                var formula8 = new Formula() {Equation = "if @0 0 @2"};
-                var formula9 = new Formula() {Equation = "if @0 @4 21600"};
-                var formula10 = new Formula() {Equation = "mid @5 @6"};
-                var formula11 = new Formula() {Equation = "mid @8 @5"};
-                var formula12 = new Formula() {Equation = "mid @7 @8"};
-                var formula13 = new Formula() {Equation = "mid @6 @7"};
-                var formula14 = new Formula() {Equation = "sum @6 0 @5"};
-
-                formulas.Append(formula1);
-                formulas.Append(formula2);
-                formulas.Append(formula3);
-                formulas.Append(formula4);
-                formulas.Append(formula5);
-                formulas.Append(formula6);
-                formulas.Append(formula7);
-                formulas.Append(formula8);
-                formulas.Append(formula9);
-                formulas.Append(formula10);
-                formulas.Append(formula11);
-                formulas.Append(formula12);
-                formulas.Append(formula13);
-                formulas.Append(formula14);
-
-                var vmlPath = new DocumentFormat.OpenXml.Vml.Path()
-                {
-                    AllowTextPath = true,
-                    ConnectionPointType = ConnectValues.Custom,
-                    ConnectionPoints = "@9,0;@10,10800;@11,21600;@12,10800",
-                    ConnectAngles = "270,180,90,0"
-                };
-                var textPath = new TextPath()
-                {
-                    On = DocumentFormat.OpenXml.TrueFalseValue.FromBoolean(true),
-                    FitShape = DocumentFormat.OpenXml.TrueFalseValue.FromBoolean(true)
-                };
-
-                var shapeHandles = new ShapeHandles();
-                var shapeHandle = new ShapeHandle
-                {
-                    Position = $"#0,{_options.Position}",
-                    XRange = "6629,14971"
-                };
-
-                shapeHandles.Append(shapeHandle);
-
-                var vmlLock = new Lock
-                {
-                    Extension = ExtensionHandlingBehaviorValues.Edit,
-                    TextLock = DocumentFormat.OpenXml.TrueFalseValue.FromBoolean(true),
-                    ShapeType = DocumentFormat.OpenXml.TrueFalseValue.FromBoolean(true)
-                };
-
-                shapeType.Append(formulas);
-                shapeType.Append(vmlPath);
-                shapeType.Append(textPath);
-                shapeType.Append(shapeHandles);
-                shapeType.Append(vmlLock);
-
-                var vmlShape = new Shape()
-                {
-                    Id = "PowerPlusWaterMarkObject357476642",
-                    Style = _options.ElementStyle,
-                    OptionalString = "_x0000_s2049",
-                    AllowInCell = DocumentFormat.OpenXml.TrueFalseValue.FromBoolean(true),
-                    FillColor = _options.ElementColor,
-                    Stroked = DocumentFormat.OpenXml.TrueFalseValue.FromBoolean(true),
-                    Type = WaterMarkTypeId
-                };
-
-                var vmlFill = new Fill() {Opacity = _options.Opacity};
-                var fonTextPath = new TextPath()
-                {
-                    Style = _options.ElementFontFamily,
-                    String = _options.Text
-                };
-
-                var textWrap = new TextWrap()
-                {
-                    AnchorX = HorizontalAnchorValues.Margin,
-                    AnchorY = VerticalAnchorValues.Margin
-                };
-
-                vmlShape.Append(vmlFill);
-                vmlShape.Append(fonTextPath);
-                vmlShape.Append(textWrap);
-                
-                picture.Append(shapeType);
-                picture.Append(vmlShape);
-                
-                run1.Append(runProperties);
-                run1.Append(picture);
-                paragraph.Append(paragraphProperties);
-                paragraph.Append(run1);
-                sdtContentBlock.Append(paragraph);
-                
-                sdtBlock.Append(sdtProperties);
-                sdtBlock.Append(sdtContentBlock);
-
-                headerPart.Header.Append(sdtBlock);
-                headerPart.Header.Save();
-            }
-        }
-
-        private Header MakeHeader()
-        {
-            var header = new Header();
-            var paragraph = new Paragraph();
-            var run = new Run();
-            var text = new Text { Text = "" };
-
-            run.Append(text);
-            paragraph.Append(run);
-            header.Append(paragraph);
-
-            return header;
         }
 
         private void RemoveWatermark(WordprocessingDocument doc)
